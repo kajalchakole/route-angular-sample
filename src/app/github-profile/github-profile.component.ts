81d4d8269399a03d1283dc124c56c3b19381e8e1
kc <kajalchakole@yahoo.co.in>
2018-04-11 19:11:07 +0530

    Added sample route project

--- src/app/github-profile/github-profile.component.ts
+++ src/app/github-profile/github-profile.component.ts
@@ -1,3 +1,4 @@
+import { ActivatedRoute, Router } from "@angular/router";
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GithubProfileComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route : Router) { }
 
   ngOnInit() {
+    // this.route.paramMap.subscribe(params => {
+    //   let id = +params.get('id');
+    //   console.log(id);
+    // });
+  }
+
+  submit(){
+    this.route.navigate(['/followers'], {
+      queryParams : { page : 1, order : 'newest'}
+    });
   }
 
 }
